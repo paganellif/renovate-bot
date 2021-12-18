12a3083a6ba30a49ac87ffbec3331f86578f5576
paganellif <filippo1paganelli@gmail.com>
2021-12-18 17:09:40 +0100

    Setup test renovate bot self hosted

---
+++ renovate-config.js
@@ -0,0 +1,48 @@
+Object.assign(process.env, {
+    GIT_AUTHOR_NAME: 'RenovateBot',
+    GIT_AUTHOR_EMAIL: 'bot@renovate.com',
+    GIT_COMMITTER_NAME: 'RenovateBot',
+    GIT_COMMITTER_EMAIL: 'bot@renovate.com',
+});
+
+module.exports = {
+    username: 'RenovateBot',
+    gitAuthor: 'Renovate Bot <bot@renovate.com>',
+    assignees: ["RenovateBot"],
+    onboarding: false,
+    labels: ["renovate"],
+    requireConfig: false,
+    automerge: true,
+    automergeType: 'pr',
+    rebaseWhen: 'auto',
+    dependencyDashboard: true,
+    dependencyDashboardApproval: true,
+    dependencyDashboardAutoclose: true,
+    trustLevel: 'high',
+    updateLockFiles: true,
+    skipInstalls: true,
+    semanticCommits: 'enabled',
+    semanticCommitsScope: '{{parentDir}}',
+    commitMessageExtra: "from v{{{fromVersion}}} to {{#if isMajor}}v{{{newMajor}}}{{else}}{{#if isSingleVersion}}v{{{toVersion}}}{{else}}{{{newValue}}}{{/if}}{{/if}}",
+    platform: 'github',
+    repositories: [
+        'paganellif/renovate-bot',
+        'paganellif/sodium',
+        'paganellif/pps-project-paganelli-2122'
+    ],
+    packageRules: [
+        {
+            description: 'lockFileMaintenance',
+            matchUpdateTypes: [
+                'pin',
+                'digest',
+                'patch',
+                'minor',
+                'major',
+                'lockFileMaintenance',
+            ],
+            dependencyDashboardApproval: false,
+            stabilityDays: 0,
+        },
+    ],
+};
