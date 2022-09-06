bbcbf1888bd8fba40e96da4dc39de19f8283d09b
Filippo Paganelli <filippo1paganelli@gmail.com>
2022-09-06 10:43:17 +0200

    Update renovate-config.js

--- renovate-config.js
+++ renovate-config.js
@@ -6,75 +6,52 @@ Object.assign(process.env, {
 });
 
 module.exports = {
-    username: 'RenovateBot',
-    gitAuthor: 'Renovate Bot <bot@renovate.com>',
-    assignees: ["RenovateBot"],
-    onboarding: false,
-    labels: ["renovate"],
-    requireConfig: false,
-    includeForks: true,
-    automerge: true,
-    automergeType: 'pr',
-    rebaseWhen: 'auto',
-    dependencyDashboard: false,
-    dependencyDashboardApproval: false,
-    dependencyDashboardAutoclose: false,
-    trustLevel: 'high',
-    updateLockFiles: true,
-    skipInstalls: true,
-    semanticCommits: 'enabled',
-    semanticCommitsScope: '{{parentDir}}',
-    commitMessageExtra: "from v{{{fromVersion}}} to {{#if isMajor}}v{{{newMajor}}}{{else}}{{#if isSingleVersion}}v{{{toVersion}}}{{else}}{{{newValue}}}{{/if}}{{/if}}",
-    platform: 'github',
-    repositories: [
-        'paganellif/renovate-bot',
-        'paganellif/angular-tracing',
-        'paganellif/gitlab-cli',
-        'paganellif/sodium',
-        'paganellif/CV',
-        'paganellif/tesi-paganelli',
-        'paganellif/psm-course-paganelli-2122'
+    "username": "RenovateBot",
+    "gitAuthor": "Renovate Bot <bot@renovate.com>",
+    "assignees": ["RenovateBot"],
+    "onboarding": false,
+    "labels": ["renovate"],
+    "requireConfig": "optional",
+    "includeForks": true,
+    "automerge": true,
+    "automergeType": "pr",
+    "rebaseWhen": "auto",
+    "dependencyDashboard": false,
+    "dependencyDashboardApproval": false,
+    "dependencyDashboardAutoclose": false,
+    "allowCustomCrateRegistries": true,
+    "allowScripts": true,
+    "exposeAllEnv": true,
+    "updateLockFiles": true,
+    "skipInstalls": true,
+    "semanticCommits": "enabled",
+    "semanticCommitsScope": "{{parentDir}}",
+    "commitMessageExtra": "from v{{{currentVersion}}} to {{#if isMajor}}v{{{newMajor}}}{{else}}{{#if isSingleVersion}}v{{{newVersion}}}{{else}}{{{newValue}}}{{/if}}{{/if}}",
+    "platform": "github",
+    "repositories": [
+        "paganellif/renovate-bot",
+        "paganellif/angular-tracing",
+        "paganellif/gitlab-cli",
+        "paganellif/sodium",
+        "paganellif/CV",
+        "paganellif/tesi-paganelli",
+        "paganellif/psm-course-paganelli-2122"
     ],
-    packageRules: [
+    "packageRules": [
         {
-            description: 'lockFileMaintenance',
-            matchUpdateTypes: [
-                'pin',
-                'digest',
-                'patch',
-                'minor',
-                'major',
-                'lockFileMaintenance',
-            ],
-            dependencyDashboardApproval: false,
-            stabilityDays: 0,
+            "description": "lockFileMaintenance",
+            "matchUpdateTypes": ["pin", "digest", "patch", "minor", "major", "lockFileMaintenance"],
+            "dependencyDashboardApproval": false,
+            "stabilityDays": 0
         },
         {
-            matchPaths: [
-                "**/Dockerfile*",
-                "**/docker-compose*.yml"
-            ],
-            groupName: "container-image",
+            "matchPaths": ["**/Dockerfile*", "**/docker-compose*.yml"],
+            "groupName": "container-image"
         },
-        {
-            matchPackagePatterns: ["^typescript"],
-            groupName: "typescript",
-        },
-        {
-            matchPackagePatterns: ["^@angular"],
-            groupName: "angular",
-        },
-        {
-            matchPackagePatterns: ["^@nrwl"],
-            groupName: "nrwl",
-        },
-        {
-            matchPackagePatterns: ["^@ngrx"],
-            groupName: "ngrx",
-        },
-        {
-            matchPackagePatterns: ["^io.ktor"],
-            groupName: "ktor",
-        }, 
-    ],
+        {"matchPackagePatterns": ["^typescript"], "groupName": "typescript"},
+        {"matchPackagePatterns": ["^@angular"], "groupName": "angular"},
+        {"matchPackagePatterns": ["^@nrwl"], "groupName": "nrwl"},
+        {"matchPackagePatterns": ["^@ngrx"], "groupName": "ngrx"},
+        {"matchPackagePatterns": ["^io.ktor"], "groupName": "ktor"}
+    ]
 };
